ecf2284417c19aa4efb6ec219e8898c92529d112
Tyler Boswell <tyler.boswell@nuix.com>
2017-01-15 13:31:06 -0500

    restructure: fix action bar creator

--- src/state/action-bar/action-bar-action-creators.js
+++ src/state/action-bar/action-bar-action-creators.js
@@ -2,15 +2,8 @@ import actionBarActionTypes from './action-bar-action-types';
 import { VISIBILITIES } from './action-bar-constants';
 
 export function updateVisibilities(pathname) {
-    return (dispatch, getState) => {
-        const state = getState();
-
-
-        if (!state.sidebar.visible) {
-            dispatch({
-                type: actionBarActionTypes.VISIBILITIES_UPDATED,
-                data: { ...VISIBILITIES[pathname] },
-            });
-        }
+    return {
+        type: actionBarActionTypes.VISIBILITIES_UPDATED,
+        data: { ...VISIBILITIES[pathname] },
     };
 }
