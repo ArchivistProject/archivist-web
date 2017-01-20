9151af35eaad2a84fd694e90bc037aa2d4bfc41b
Dung Mai <dqm23@drexel.edu>
2017-01-20 13:46:25 -0500

    started to rearrange layout

--- src/views/upload/components/upload.js
+++ src/views/upload/components/upload.js
@@ -1,4 +1,8 @@
 import React, { PropTypes, Component } from 'react';
+import {
+    Modal, Button, FormControl, Collapse, Well, Jumbotron, Panel, FormGroup,
+    Col, Form, ControlLabel, MenuItem, Row
+} from 'react-bootstrap/lib/';
 import './upload.scss';
 
 export default class Upload extends Component {
@@ -23,35 +27,69 @@ export default class Upload extends Component {
             (
                 <div className='upload-metadata-input' key={id}>
                     <span className='upload-label'>{field.label}</span>
-                    <input type={field.type} />
+                    <FormControl type={field.type} />
                 </div>
             )
         );
+
+
+/* put this somewhere if you want to generate these
+        {this.renderMetadataInput([
+            {
+                label: 'Title',
+                type: 'text',
+            },
+            {
+                label: 'Author',
+                type: 'text',
+            },
+            {
+                label: 'Date Published',
+                type: 'date',
+            },
+        ])}
+        */
     }
 
     render() {
         return (
             <div className='upload'>
+                <h2 className="uploadTitle">Upload New File</h2>
                 <div className='upload-content'>
                     <div className='upload-file-upload'>
-                        <span className='upload-label'>File</span>
-                        <input type='file' accept='.pdf, .html' onChange={this.handleFileChange} />
+                        <span className='upload-label'>Choose A File*</span>
+                        <br/>
+                        <input type='file' accept='.pdf, .html' onChange={this.handleFileChange}/>
                     </div>
-                    {this.renderMetadataInput([
-                        {
-                            label: 'Title',
-                            type: 'text',
-                        },
-                        {
-                            label: 'Author',
-                            type: 'text',
-                        },
-                        {
-                            label: 'Date Published',
-                            type: 'date',
-                        },
-                    ])}
-                    <button className='upload-submit' onClick={this.handleSubmit}>Upload</button>
+                    <Row>
+                    <Col sm={5}>
+                        <ControlLabel>Title*</ControlLabel>
+                        <FormControl type="text"></FormControl>
+                    </Col>
+                    <Col sm={7}>
+                        <ControlLabel>Item Type*</ControlLabel>
+                        <FormGroup controlId="formControlsSelect">
+                            <FormControl componentClass="select" placeholder="select field type">
+                                <option value="other" onSelect={this.renderMetadataInput([
+                                    {
+                                        label: 'Title',
+                                        type: 'text',
+                                    },
+                                    {
+                                        label: 'Author',
+                                        type: 'text',
+                                    },
+                                    {
+                                        label: 'Date Published',
+                                        type: 'date',
+                                    },
+                                ])}>Website</option>
+                                <option value="other">Magazine</option>
+                            </FormControl>
+                        </FormGroup>
+                    </Col>
+                    </Row>
+                    <Button onClick={this.handleSubmit}>Upload</Button>
                 </div>
             </div>
         );
