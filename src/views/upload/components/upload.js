b750a50e17d01009ed06baaa1687b1fd4d611934
Dung Mai <dqm23@drexel.edu>
2017-01-24 17:35:00 -0500

    Added Tag Box

--- src/views/upload/components/upload.js
+++ src/views/upload/components/upload.js
@@ -1,9 +1,11 @@
-import React, { PropTypes, Component } from 'react';
+import React, {PropTypes, Component} from 'react';
 import {
     Modal, Button, FormControl, Collapse, Well, Jumbotron, Panel, FormGroup,
     Col, Form, ControlLabel, MenuItem, Row
 } from 'react-bootstrap/lib/';
+import TagsInput from 'react-tagsinput';
 import './upload.scss';
+
 
 export default class Upload extends Component {
 
@@ -13,12 +15,12 @@ export default class Upload extends Component {
     };
 
     handleFileChange = (file) => {
-        const { updateUploadFile } = this.props;
+        const {updateUploadFile} = this.props;
         updateUploadFile(file);
     }
 
     handleSubmit = () => {
-        const { submitFile } = this.props;
+        const {submitFile} = this.props;
         submitFile();
     }
 
@@ -27,71 +29,72 @@ export default class Upload extends Component {
             (
                 <div className='upload-metadata-input' key={id}>
                     <span className='upload-label'>{field.label}</span>
-                    <FormControl type={field.type} />
+                    <FormControl type={field.type}/>
                 </div>
             )
         );
-
-
-/* put this somewhere if you want to generate these
-        {this.renderMetadataInput([
-            {
-                label: 'Title',
-                type: 'text',
-            },
-            {
-                label: 'Author',
-                type: 'text',
-            },
-            {
-                label: 'Date Published',
-                type: 'date',
-            },
-        ])}
-        */
     }
 
     render() {
         return (
-            <div className='upload'>
-                <h2 className="uploadTitle">Upload New File</h2>
-                <div className='upload-content'>
-                    <div className='upload-file-upload'>
-                        <span className='upload-label'>Choose A File*</span>
-                        <br/>
-                        <input type='file' accept='.pdf, .html' onChange={this.handleFileChange}/>
+            <div className="upload-content">
+                <div>
+                    <h2 className="upload-title">Upload New File</h2>
+                    <div>
+                        <div className='upload-file-upload'>
+                            <ControlLabel>Choose A File*</ControlLabel>
+                            <br/>
+                            <input type='file' accept='.pdf, .html' onChange={this.handleFileChange}/>
+                        </div>
+                        <Row>
+                            <Col sm={5}>
+                                <ControlLabel>Title*</ControlLabel>
+                                <FormControl type="text"></FormControl>
+                            </Col>
+                            <Col sm={7}>
+                                <ControlLabel>Item Type*</ControlLabel>
+                                <FormGroup controlId="formControlsSelect">
+                                    <FormControl componentClass="select" placeholder="select field type">
+                                        <option value="other">Website</option>
+                                        <option value="other">Magazine</option>
+                                    </FormControl>
+                                </FormGroup>
+                            </Col>
+                        </Row>
                     </div>
-                    <Row>
-                    <Col sm={5}>
-                        <ControlLabel>Title*</ControlLabel>
-                        <FormControl type="text"></FormControl>
-                    </Col>
-                    <Col sm={7}>
-                        <ControlLabel>Item Type*</ControlLabel>
-                        <FormGroup controlId="formControlsSelect">
-                            <FormControl componentClass="select" placeholder="select field type">
-                                <option value="other" onSelect={this.renderMetadataInput([
-                                    {
-                                        label: 'Title',
-                                        type: 'text',
-                                    },
-                                    {
-                                        label: 'Author',
-                                        type: 'text',
-                                    },
-                                    {
-                                        label: 'Date Published',
-                                        type: 'date',
-                                    },
-                                ])}>Website</option>
-                                <option value="other">Magazine</option>
-                            </FormControl>
-                        </FormGroup>
-                    </Col>
-                    </Row>
+                </div>
+                <div>
+                    <ControlLabel>Tags:</ControlLabel>
+                    <TagBox/>
+                    <br/>
+                    <br/>
                     <Button onClick={this.handleSubmit}>Upload</Button>
                 </div>
             </div>
         );
     }
 }
+
+//Create the tag box
+const TagBox = React.createClass({
+
+    getInitialState(){
+        return {tags: []}
+    },
+
+    handleChange(value){
+        this.setState({tags: value});
+
+        for (let i = 0; i <= this.state.tags.length; i++)
+            console.log(this.state.tags[i]);
+
+    },
+
+    render(){
+        return (
+            <div>
+                <TagsInput value={this.state.tags} onChange={this.handleChange}/>
+            </div>
+        );
+    }
+})
