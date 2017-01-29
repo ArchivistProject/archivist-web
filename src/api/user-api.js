f53a82f05b51a900de32b83dc66db13c9b04b68d
Tyler Boswell <tjb322@drexel.edu>
2017-01-29 15:33:39 -0500

    login: use field values

--- src/api/user-api.js
+++ src/api/user-api.js
@@ -3,8 +3,8 @@ import $ from 'jquery';
 
 export function login(username, password) {
     const payload = {
-        email: 'foo@example.com',
-        password: 'foo',
+        email: username,
+        password,
     };
     $.post(`${config.backend}/authentication/login`, payload);
 }
