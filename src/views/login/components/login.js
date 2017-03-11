31365a7e7fb76e61b3a7342b1cc18d692857c2b4
Dung Mai <dqm23@drexel.edu>
2017-03-11 12:53:16 -0500

    dont show "logout" button on actionbar if not logged in yet

--- src/views/login/components/login.js
+++ src/views/login/components/login.js
@@ -1,6 +1,6 @@
-import React, {PropTypes, Component} from 'react';
-import './login.scss';
+import React, { PropTypes, Component } from 'react';
 import Logo from '~/src/assets/images/logo.png';
+import './login.scss';
 
 export default class Login extends Component {
 
@@ -14,55 +14,61 @@ export default class Login extends Component {
     };
 
     handleUsernameChanged = (e) => {
-        const {usernameChanged} = this.props;
+        const { usernameChanged } = this.props;
         usernameChanged(e.target.value);
     }
 
     handlePasswordChanged = (e) => {
-        const {passwordChanged} = this.props;
+        const { passwordChanged } = this.props;
         passwordChanged(e.target.value);
     }
 
     handleLoginClicked = () => {
-        const {login, usernameField, passwordField} = this.props;
+        const { login, usernameField, passwordField } = this.props;
         login(usernameField, passwordField);
     }
 
 
     render() {
-        const {usernameField, passwordField} = this.props;
+        const { usernameField, passwordField } = this.props;
         return (
-                <div className='main'>
-                    <div className='col-lg-12'>
-                        <div className='container'>
-                            <div className='col-lg-4'></div>
-                            <div className='col-lg-4'>
-                                <div className='row grids text-center'>
-                                    <div className='view view-tenth'>
-                                        <div className='inner_content clearfix'>
-                                            <div className='product_image'>
-                                                <img
-                                                    src={Logo}
-                                                    className='img-responsive' alt='archivist logo'/>
-                                            </div>
-                                            <div className='label-product'>
-                                                <span className='new'>LOGIN</span>
-                                            </div>
-                                            <div className='mask'>
-                                                <h2>Welcome To Archivist</h2>
-                                                <div className='main'>
-                                                    <form>
-                                                        <input type='text' className='text' value={usernameField}
-                                                               onChange={this.handleUsernameChanged}
-                                                               placeholder='username' />
-                                                        <input type='password' placeholder='Password'
-                                                               value={passwordField}
-                                                               onChange={this.handlePasswordChanged} />
-                                                        <div className='submit'><input type='submit' value='Sign in'
-                                                                                       onClick={this.handleLoginClicked} />
-                                                        </div>
-                                                    </form>
-                                                </div>
+            <div className='main'>
+                <div className='col-lg-12'>
+                    <div className='container'>
+                        <div className='col-lg-4' />
+                        <div className='col-lg-4'>
+                            <div className='row grids text-center'>
+                                <div className='view view-tenth'>
+                                    <div className='inner_content clearfix'>
+                                        <div className='product_image'>
+                                            <img
+                                                src={Logo}
+                                                className='img-responsive' alt='archivist logo'
+                                            />
+                                        </div>
+                                        <div className='label-product'>
+                                            <span className='new'>LOGIN</span>
+                                        </div>
+                                        <div className='mask'>
+                                            <h2>Welcome To Archivist</h2>
+                                            <div className='main'>
+                                                <form>
+                                                    <input
+                                                        type='text' className='text' value={usernameField}
+                                                        onChange={this.handleUsernameChanged}
+                                                        placeholder='username'
+                                                    />
+                                                    <input
+                                                        type='password' placeholder='Password'
+                                                        value={passwordField}
+                                                        onChange={this.handlePasswordChanged}
+                                                    />
+                                                    <div className='submit'><input
+                                                        type='submit' value='Login'
+                                                        onClick={this.handleLoginClicked}
+                                                    />
+                                                    </div>
+                                                </form>
                                             </div>
                                         </div>
                                     </div>
@@ -71,6 +77,7 @@ export default class Login extends Component {
                         </div>
                     </div>
                 </div>
+            </div>
         );
     }
 }
