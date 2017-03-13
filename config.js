26efa691df5afbc5f67e6f2f888fdc9a4d56d6d2
Charles <gilliamcc@gmail.com>
2017-03-12 23:49:35 -0400

    We are now using SSL when connecting to the api

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export default {
-    backend: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : window.location.origin,
+    backend: process.env.NODE_ENV === 'development' ? 'https://localhost:3000' : window.location.origin,
 };
