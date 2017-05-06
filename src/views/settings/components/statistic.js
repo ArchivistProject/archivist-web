7b5ed921e3829d53f8ef80a9c40b9bf461418208
Dung Mai <dungqmai@yahoo.com>
2017-05-06 14:10:41 -0400

    added truncate to GB and changed text

--- src/views/settings/components/statistic.js
+++ src/views/settings/components/statistic.js
@@ -19,7 +19,8 @@ export default class Statistic extends Component {
         const storageInMB = (storageInBytes / 1000000); // 1 million bytes in a MB
 
         if (storageInMB > 500) { // More than 500 MB, lets show storage in GB then
-            return `${storageInMB / 1000} GB`; // 1000 MB in GB
+            const roundedStorageInGB = Number(storageInMB / 1000).toFixed(2);
+            return `${roundedStorageInGB} GB`; // 1000 MB in GB
         }
 
         const roundedStorageMB = Number(storageInMB).toFixed(2);
@@ -36,7 +37,7 @@ export default class Statistic extends Component {
             <div>
                 <p className='settings-label'>Usage</p>
                 <p><b>{fileCount} files</b> in Archivist</p>
-                <p><b>{humanReadableStorage}</b> of memory</p>
+                <p><b>{humanReadableStorage}</b> of disk space used for files</p>
                 <br />
             </div>
 
