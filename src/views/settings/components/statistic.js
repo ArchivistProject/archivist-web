aec78b5d229820fd9c49b76da8d319e0c3491df9
Dung <dungqmai23@gmail.com>
2017-05-06 22:21:50 -0400

    ARC-195 & 199: Changed statistic to usage & truncate size (#32)
    * changed statistic to usage & truncate size
    
    * added truncate to GB and changed text

--- src/views/settings/components/statistic.js
+++ src/views/settings/components/statistic.js
@@ -19,10 +19,12 @@ export default class Statistic extends Component {
         const storageInMB = (storageInBytes / 1000000); // 1 million bytes in a MB
 
         if (storageInMB > 500) { // More than 500 MB, lets show storage in GB then
-            return `${storageInMB / 1000} GB`; // 1000 MB in GB
+            const roundedStorageInGB = Number(storageInMB / 1000).toFixed(2);
+            return `${roundedStorageInGB} GB`; // 1000 MB in GB
         }
 
-        return `${storageInMB} MB`;
+        const roundedStorageMB = Number(storageInMB).toFixed(2);
+        return `${roundedStorageMB} MB`;
     }
 
     render() {
@@ -33,9 +35,9 @@ export default class Statistic extends Component {
         return (
 
             <div>
-                <p className='settings-label'>Statistic</p>
-                <p><b>{fileCount} files</b> uploaded</p>
-                <p><b>{humanReadableStorage}</b> of files size</p>
+                <p className='settings-label'>Usage</p>
+                <p><b>{fileCount} files</b> in Archivist</p>
+                <p><b>{humanReadableStorage}</b> of disk space used for files</p>
                 <br />
             </div>
 
