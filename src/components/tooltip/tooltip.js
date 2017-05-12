63917ff6cde15cb3eec5bd77961d9b26633e261e
Charles <gilliamcc@gmail.com>
2017-05-12 16:49:35 -0400

    Add multiple directions for the tooltips to pop up in

--- src/components/tooltip/tooltip.js
+++ src/components/tooltip/tooltip.js
@@ -3,22 +3,34 @@ import './tooltip.scss';
 
 export default class Tooltip extends Component {
 
+    static DIRECTION = {
+        top: 'top',
+        bottom: 'bottom',
+        left: 'left',
+        right: 'right',
+    };
+
     static MESSAGES = {
         tagEntry: 'Press the enter key to submit the tag.',
         dateAdded: 'Date Added',
     };
 
     static propTypes = {
+        direction: PropTypes.string,
         message: PropTypes.string,
     };
 
+    static defaultProps = {
+        direction: 'top',
+    };
+
     render() {
-        const { message } = this.props;
+        const { message, direction } = this.props;
 
         return (
             <div className='tooltip2'>
                 <div className='questionmark'>?</div>
-                <span className='tooltiptext'>{message}</span>
+                <span className={`tooltiptext ${direction}`}>{message}</span>
             </div>
         );
     }
