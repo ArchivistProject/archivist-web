fd6fe55c3391754d7497ea0f0b31ce13ccf2b556
Charles <gilliamcc@gmail.com>
2017-05-16 17:32:53 -0400

    Improved the look of the notification

--- src/state/viewer/viewer-action-creators.js
+++ src/state/viewer/viewer-action-creators.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import viewerActionTypes from './viewer-action-types';
 
 export function updateScale(increment) {
@@ -32,29 +33,47 @@ export function viewerClosed() {
 
 export function linkClicked(url) {
     return (dispatch) => {
+        const notificationLinkParaStyle = {
+            whiteSpace: 'nowrap',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+        };
+        const notificationButtonStyle = {
+            background: 'rgb(235, 173, 26)',
+            borderRadius: '2px',
+            padding: '6px 20px',
+            fontWeight: 'bold',
+            margin: '10px 0px 0px',
+            border: '0px',
+            color: 'rgb(255, 255, 255)',
+        };
+        const notificationButtonCallback = function() {
+            var win = window.open(url, '_blank');
+            if (win) {
+                win.focus();
+            } else {
+                dispatch({
+                    type: viewerActionTypes.POPUPS_DISABLED,
+                    notification: {
+                        title: 'Please enable popups on this site to visit this link.',
+                        level: 'error',
+                    }
+                });
+            }
+        };
         dispatch({
             type: viewerActionTypes.LINK_CLICKED,
             notification: {
-                title: 'Clicking outside link',
-                message: `Are you sure you want to visit ${url}`,
+                title: 'Clicked outside link',
                 level: 'warning',
-                action: {
-                    label: 'Yes',
-                    callback: function() {
-                        var win = window.open(url, '_blank');
-                        if (win) {
-                            win.focus();
-                        } else {
-                            dispatch({
-                                type: viewerActionTypes.POPUPS_DISABLED,
-                                notification: {
-                                    title: 'Please enable popups on this site to visit this link.',
-                                    level: 'error',
-                                }
-                            });
-                        }
-                    },
-                },
+                children: (
+                    <div>
+                        Are you sure you want to visit:
+                        <p style={notificationLinkParaStyle} title={url}>{url}</p>
+                        <button style={notificationButtonStyle} onClick={notificationButtonCallback}>Yes</button>
+                    </div>
+                ),
+                autoDismiss: 0,
             },
         });
     };
